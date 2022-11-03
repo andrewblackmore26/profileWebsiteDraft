26e88f96ca57444f8384fe48189c69e8072b0588
MrBlackmo <andrewblackmore95@gmail.com>
2022-11-04 10:02:50 +1100

    stuck somewhere

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import {useState, useEffect} from "react"
-import logo from '../assets/img/logo'
+import logo from '../assets/img/logo.svg'
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
 import navIcon3 from '../assets/img/nav-icon3.svg'
 
-export const NavBar = () => {
+function NavBar() {
   const [activeLink, setActiveLink] = useState('home')
   const [scrolled, setScrolled] = useState(false)
 
@@ -47,9 +46,9 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="" /></a>
-              <a href="#"><img src={navIcon2} alt="" /></a>
-              <a href="#"><img src={navIcon3} alt="" /></a>
+              <a href="#home"><img src={navIcon1} alt="" /></a>
+              <a href="#home"><img src={navIcon2} alt="" /></a>
+              <a href="#home"><img src={navIcon3} alt="" /></a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
           </span>
@@ -58,3 +57,5 @@ export const NavBar = () => {
     </Navbar>
   )
 }
+
+export default NavBar;
