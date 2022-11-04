70d2480ff852236018e2331f792e93b67b62cffd
MrBlackmo <andrewblackmore95@gmail.com>
2022-11-04 17:19:43 +1100

    Banner added, looks real now

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Banner from './Banner';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Banner />
+    
   </React.StrictMode>
 );
 
